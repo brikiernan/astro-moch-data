ea565128ade266333904dc6071db9568ed726a27
Brian <brian@briankiernan.com>
2022-07-20 01:41:09 -0400

    add cors

--- functions/api.ts
+++ functions/api.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import serverless from 'serverless-http';
+import cors from 'cors';
 
 const app = express();
 const v1 = express.Router();
+
+app.use(cors());
 
 v1.get('/contacts', async (_req, res) => {
   const contacts = await import('../data/contacts');
